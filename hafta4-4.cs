fa014ab523e07cab82ee5848ec723864581e1c0a
smelikeaslan <aslansumeyramelike@gmail.com>
2025-01-09 16:16:15 +0300

    Add files via upload

---
+++ hafta4-4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fibonacci dizisinde kaçıncı sayıyı görmek istiyorsunuz? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int sonuc = Fibonacci(n);
+            Console.WriteLine($"{n}. Fibonacci sayısı: {sonuc}");
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            Console.ReadKey();
+        }
+    }
+}
